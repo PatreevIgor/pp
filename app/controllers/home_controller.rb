4086a111b5a5c7abdbc8c4de0310bedc8c40ce03
PatreevIgor <igormanxxxx@yandex.ru>
2016-10-29 12:27:55 +0300

    add list rooms and list users to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+
   def index
+  	@rooms = Room.all
+  	@users = User.all
   end
+
+  def invite_user
+  end
+
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ActiveRecord::Base
+	def invite_to_room
+		
+	end
+
 end
