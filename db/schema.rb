bfe8b4aeab92798d3ab7b9c8149516e6a02252a1
PatreevIgor <igormanxxxx@yandex.ru>
2016-10-29 11:42:55 +0300

    add scaffold room, add code to rooms_controller (when you create an article entry the field creater of the current user)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029074640) do
+ActiveRecord::Schema.define(version: 20161029082723) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "title"
+    t.string   "creater"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
